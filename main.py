79be62daa0c62c023a0ab09c12370fca674483b2
Diana Lam <DLam01@fgfbrands.com>
2024-06-04 12:39:12 -0400

    Uploads files to root

--- main.py
+++ main.py
@@ -2,41 +2,66 @@ from text_extract import sort_text
 from parse import parse_text
 import pandas as pd
 import xlsxwriter
+from flask import Flask, render_template, request
 
-pdf_file = r'C:\Users\dlam01\OneDrive - FGF Brands Inc\Desktop\projects\InvoiceDataExtractor\invoices\example.pdf'
+app = Flask(__name__)
 
-# Extracts text from pdf horizontally
-text = sort_text(pdf_file)
+@app.route('/')
+def main():
+    return render_template("index.html")
 
-# Returns dict -> df 
-invoice_dict = parse_text(text)
-invoice_df = pd.DataFrame([invoice_dict])
+@app.route('/upload', methods=['POST'])
+def upload():
+    if request.method == 'POST':
+        # Gets the list of files from webpage
+        file_list = request.files.getlist("file")
+
+        # Iterate for reach file in list
+        for file in file_list:
+            file.save(file.filename)
+        return "Files uploaded Sucessfully!"
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
+    app.run(debug = True)
 
 
 
-# Create a Pandas Excel writer
-file_name = 'example.xlsx'
-writer = pd.ExcelWriter(file_name, engine='xlsxwriter')
-# Convert to Excel object
-invoice_df.to_excel(writer, sheet_name='Sheet1', index=False, startrow=1, header=False)
-# Get the xlsxwriter workbook and worksheet objects.
-workbook = writer.book
-worksheet = writer.sheets['Sheet1']
+# pdf_file = r'C:\Users\dlam01\OneDrive - FGF Brands Inc\Desktop\projects\InvoiceDataExtractor\invoices\example.pdf'
 
-# Header format
-header_format = workbook.add_format(
-    {
-        'bold': True,
-        'text_wrap': True,
-        'valign': "top",
-        'border': 1,
-    }
-)
+# # Extracts text from pdf horizontally
+# text = sort_text(pdf_file)
 
-for col_num, value in enumerate(invoice_df.columns.values):
-    worksheet.write(0, col_num, value, header_format)
+# # Returns dict -> df 
+# invoice_dict = parse_text(text)
+# invoice_df = pd.DataFrame([invoice_dict])
 
-writer.close()
+
+
+# # Create a Pandas Excel writer
+# file_name = 'example.xlsx'
+# writer = pd.ExcelWriter(file_name, engine='xlsxwriter')
+# # Convert to Excel object
+# invoice_df.to_excel(writer, sheet_name='Sheet1', index=False, startrow=1, header=False)
+# # Get the xlsxwriter workbook and worksheet objects.
+# workbook = writer.book
+# worksheet = writer.sheets['Sheet1']
+
+# # Header format
+# header_format = workbook.add_format(
+#     {
+#         'bold': True,
+#         'text_wrap': True,
+#         'valign': "top",
+#         'border': 1,
+#     }
+# )
+
+# for col_num, value in enumerate(invoice_df.columns.values):
+#     worksheet.write(0, col_num, value, header_format)
+
+# writer.close()
 
 
 
